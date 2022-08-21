143916068e396100b43becbe864e2ca20cd0cff2
Mohammad Bijantium Sinatria <79687063+Bintaaaa@users.noreply.github.com>
2022-08-21 23:04:44 +0700

    styling

---
+++ shared_libraries/common/lib/constants/app_color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  const AppColor();
+
+  /// Color: #BDBDBD
+  static const Color outlineSecondaryColor = Color(0xFFBDBDBD);
+
+  /// Color: #E96945
+  static const Color primaryColor = Color(0xFFE96945);
+
+  /// Color: #EDD4CD
+  static const Color primaryColorSoft = Color(0xFFEDD4CD);
+
+  /// Color: #FFFFFF
+  static const Color secondaryColor = Color(0xFFFFFFFF);
+
+  /// Color: #5A5A5A
+  static const Color subtitleColor = Color(0xFF5A5A5A);
+
+  /// Color: #121212
+  static const Color titleColor = Color(0xFF121212);
+}
---
+++ shared_libraries/common/lib/constants/app_text_style.dart
@@ -0,0 +1,128 @@
+import 'package:common/constants/app_color.dart';
+import 'package:dependencies/google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  const AppTextStyle();
+  static const FontWeight titleFontWeigth = FontWeight.w700;
+  static const FontWeight subtitleFontWeigth = FontWeight.w600;
+  static const FontWeight bodyFontWeigth = FontWeight.w400;
+
+  static TextStyle kHeading1 = GoogleFonts.dmSans(
+    fontSize: 48.0,
+    color: AppColor.subtitleColor,
+    fontWeight: titleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kHeading2 = GoogleFonts.dmSans(
+    fontSize: 36.0,
+    color: AppColor.subtitleColor,
+    fontWeight: titleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kHeading3 = GoogleFonts.dmSans(
+    fontSize: 32.0,
+    color: AppColor.subtitleColor,
+    fontWeight: titleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kHeading4 = GoogleFonts.dmSans(
+    fontSize: 26.0,
+    color: AppColor.subtitleColor,
+    fontWeight: titleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kHeading5 = GoogleFonts.dmSans(
+    fontSize: 22.0,
+    color: AppColor.subtitleColor,
+    fontWeight: titleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kHeading6 = GoogleFonts.dmSans(
+    fontSize: 18.0,
+    color: AppColor.subtitleColor,
+    fontWeight: titleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kSubtutle1 = GoogleFonts.dmSans(
+    fontSize: 22.0,
+    color: AppColor.subtitleColor,
+    fontWeight: subtitleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kSubtutle2 = GoogleFonts.dmSans(
+    fontSize: 18.0,
+    color: AppColor.subtitleColor,
+    fontWeight: subtitleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kSubtutle3 = GoogleFonts.dmSans(
+    fontSize: 14.0,
+    color: AppColor.subtitleColor,
+    fontWeight: subtitleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kSubtutle4 = GoogleFonts.dmSans(
+    fontSize: 12.0,
+    color: AppColor.subtitleColor,
+    fontWeight: subtitleFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kBody1 = GoogleFonts.dmSans(
+    fontSize: 14.0,
+    color: AppColor.subtitleColor,
+    fontWeight: bodyFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kBody2 = GoogleFonts.dmSans(
+    fontSize: 14.0,
+    color: AppColor.subtitleColor,
+    fontWeight: bodyFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kButton1 = GoogleFonts.dmSans(
+    fontSize: 22.0,
+    color: AppColor.secondaryColor,
+    fontWeight: bodyFontWeigth,
+    letterSpacing: 0.15,
+  );
+  static TextStyle kButton2 = GoogleFonts.dmSans(
+    fontSize: 22.0,
+    color: AppColor.primaryColor,
+    fontWeight: bodyFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kLabel1 = GoogleFonts.dmSans(
+    fontSize: 18.0,
+    color: AppColor.titleColor,
+    fontWeight: bodyFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kLabel2 = GoogleFonts.dmSans(
+    fontSize: 16.0,
+    color: AppColor.titleColor,
+    fontWeight: bodyFontWeigth,
+    letterSpacing: 0.15,
+  );
+
+  static TextStyle kLabel3 = GoogleFonts.dmSans(
+    fontSize: 12.0,
+    color: AppColor.titleColor,
+    fontWeight: bodyFontWeigth,
+    letterSpacing: 0.15,
+  );
+}
