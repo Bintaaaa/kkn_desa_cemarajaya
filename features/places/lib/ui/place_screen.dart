4ebb635148d07f57b71a433830518c404501cc82
Mohammad Bijantium Sinatria <79687063+Bintaaaa@users.noreply.github.com>
2022-08-23 12:31:12 +0700

    assets n ui place screen

--- features/places/lib/ui/place_screen.dart
+++ features/places/lib/ui/place_screen.dart
@@ -1,3 +1,5 @@
+import 'package:common/constants/app_color.dart';
+import 'package:common/constants/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class PlaceScreen extends StatelessWidget {
@@ -7,7 +9,135 @@ class PlaceScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text("ini place"),
+        child: Container(
+          margin: const EdgeInsets.all(18.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                titleHeadingPlace(),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                Container(
+                  height: 200,
+                  padding: const EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.only(bottom: 12.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.blackColor,
+                    image: const DecorationImage(
+                      image: AssetImage(
+                        'assets/place.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      18.0,
+                    ),
+                  ),
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Pantai Di Desa Cemarajaya',
+                    style: AppTextStyle.kHeading4.copyWith(
+                      color: AppColor.secondaryColor,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  padding: const EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.only(bottom: 12.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.blackColor,
+                    image: const DecorationImage(
+                      image: AssetImage(
+                        'assets/place.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      18.0,
+                    ),
+                  ),
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Pantai Di Desa Cemarajaya',
+                    style: AppTextStyle.kHeading4.copyWith(
+                      color: AppColor.secondaryColor,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  padding: const EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.only(bottom: 12.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.blackColor,
+                    image: const DecorationImage(
+                      image: AssetImage(
+                        'assets/place.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      18.0,
+                    ),
+                  ),
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Pantai Di Desa Cemarajaya',
+                    style: AppTextStyle.kHeading4.copyWith(
+                      color: AppColor.secondaryColor,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  padding: const EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.only(bottom: 12.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.blackColor,
+                    image: const DecorationImage(
+                      image: AssetImage(
+                        'assets/place.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      18.0,
+                    ),
+                  ),
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Pantai Di Desa Cemarajaya',
+                    style: AppTextStyle.kHeading4.copyWith(
+                      color: AppColor.secondaryColor,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  RichText titleHeadingPlace() {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Wisata Alam\n',
+            style: AppTextStyle.kHeading3,
+          ),
+          TextSpan(
+            text: 'Cemarajaya',
+            style: AppTextStyle.kHeading3.copyWith(
+              color: AppColor.primaryColor,
+            ),
+          )
+        ],
       ),
     );
   }
